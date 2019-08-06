4ffc23d5a4a57e5ef55ceb5ca2b1c0634a3328ac
hphucs <chphuc05@gmail.com>
2019-08-06 14:52:30 +0000

    add func

--- news/newsServer.php
+++ news/newsServer.php
@@ -1,23 +1,35 @@
 <?php
 $url = "http://ip-api.com/json/";
-echo $_POST["task"]."\n";
+
+function getLoc(){
+    global $url;
+    $address = $_SERVER['REMOTE_ADDR'];
+    $local = curl_init();
+    $finalURL = $url.$address;
+    curl_setopt($local, CURLOPT_URL, $finalURL);
+    //curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($local,CURLOPT_RETURNTRANSFER, true);
+    //echo curl_exec($local);
+    //echo $finalURL;
+    $result = json_decode(curl_exec($local), true);
+    curl_close($local);
+    $respone = [
+        "lat" => $result["lat"],
+        "lon" => $result["lon"],
+    ];
+    echo json_encode($respone);
+}
+
 if ($_POST["task"] == "getIP"){
     echo $_SERVER['REMOTE_ADDR'];
 }
 if ($_POST["task"] == "getLocation"){
     
-    $address = $_SERVER['REMOTE_ADDR'];
+    getLoc();
+}
+if ($_POST["task"] == "getWeather"){
+    $loc = json_decode(getLoc(), true);
     
-    $local = curl_init();
-    
-    $finalURL = $url.$address;
-    echo $finalURL;
-    curl_setopt($local, CURLOPT_URL, $finalURL);
-    //curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
-    curl_setopt($local,CURLOPT_RETURNTRANSFER, true);
-    $result = curl_exec($local);
-    echo $result;
-    //echo "hi";
 }
 
 ?>
