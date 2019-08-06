9c0dc65cd7f25fd3a034d5819acb05a56a0426d1
HoangPhuc05 <chphuc05@gmail.com>
2019-08-06 08:18:40 +0700

    8/6/2019 add home page

---
+++ script/home.js
@@ -0,0 +1,51 @@
+
+function fadeOut(mid){
+    
+    console.log(mid);
+    mid.classList.add("animated", "fadeOutUp");
+    mid.addEventListener('animationend', function() { 
+        while (mid.firstChild) {
+            mid.removeChild(mid.firstChild);   
+        }
+        addMain(mid);
+    })
+}
+
+function addMain(mid){
+    var $mid = $(mid)
+    $mid.removeClass("animated fadeOutUp");
+    mid = $("#inner");
+    contents = '<div class="row">';
+    //contents += content;
+    //contents += content
+    contents += '</div>';
+    mid.append($additionContent);
+    $additionContent.on('animationend', function(){
+        $additionContent.removeClass('animated fadeInUp');
+    })
+
+    console.log("Aa");
+    
+} 
+
+$(document).ready(function(){
+    var allCard = $(".card");
+    for(var i = 0; i < allCard.length ; i++){
+        allCard.eq(i).attr("onmouseover","mouseOverAni(this);");
+    }
+
+    $additionContent = $("#addition")
+    $additionContent.remove(); 
+})
+
+function mouseOverAni(obj){
+    if (obj == obj.parent()){
+        return;
+    }
+    obj.classList.add("animated", "bounce");
+    obj.addEventListener('animationend',function(){
+        $(obj).removeClass('animated bounce');
+    });
+    
+}
+
